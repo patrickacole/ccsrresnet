376f55eb5e4bc20f04303310b16fceadef03a091
Patrick Cole <patrickacole@yahoo.com>
2020-03-07 00:46:13 -0600

    Adding dataset that adds noise to images for NIH

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import skimage
 import numpy as np
 
 from PIL import Image
@@ -11,15 +12,14 @@ from torchvision.transforms import ToTensor
 
 
 class CXR8Dataset(Dataset):
-    def __init__(self, root, image_shape=(256, 256), scale_factor=4, color='grayscale', upsample='bicubic'):
+    def __init__(self, root, image_shape=(256, 256), scale_factor=4, add_noise=True):
         if not os.path.exists(root):
             raise OSError("{}, path does not exist..".format(root))
 
         self.root = root
         self.image_shape = np.asarray(image_shape)
         self.scale_factor = scale_factor
-        self.color = color
-        self.upsample = upsample
+        self.add_noise = add_noise
         self.samples = [sample for sample in os.listdir(root) if '.png' in sample]
         self.totensor = ToTensor()
 
@@ -27,22 +27,15 @@ class CXR8Dataset(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        # Use gray scale for now
-        image = Image.open(self.at(idx))
+        image = Image.open(self.at(idx)).convert('L')
         imageHR = image.resize(self.image_shape[::-1], resample=Image.LANCZOS)
         imageLR = image.resize(self.image_shape[::-1] // self.scale_factor, resample=Image.LANCZOS)
-        if self.upsample == 'bicubic':
-            imageLR = imageLR.resize(self.image_shape[::-1], resample=Image.BICUBIC)
-        elif self.upsample == 'bilinear':
-            imageLR = imageLR.resize(self.image_shape[::-1], resample=Image.BILINEAR)
 
-        if self.color == 'ycbcr':
-            yLR, ycbcrLR = preprocess(imageLR)
-            yHR, ycbcrHR = preprocess(imageHR)
-            return (yLR, yHR)
-        elif self.color == 'grayscale':
-            imageLR = imageLR.convert('L')
-            imageHR = imageHR.convert('L')
+        if self.add_noise:
+            imageLR = np.asarray(imageLR)
+            imageLR = np.random.poisson(imageLR)
+            imageLR = np.clip(imageLR, 0, 255).astype(np.uint8)
+            imageLR = Image.fromarray(imageLR)
 
         return (self.totensor(imageLR), self.totensor(imageHR))
 
@@ -77,10 +70,11 @@ class NoisyXrayDataset(Dataset):
 
 
 if __name__=="__main__":
-    which = 'Noisy'
-    if which == 'CX48':
-        datapath = os.path.expanduser("~/Projects/datasets/CXR8/images/")
-        dataset = CXR8Dataset(datapath, color="grayscale")
+    # which = 'Noisy'
+    which = 'CXR8'
+    if which == 'CXR8':
+        datapath = os.path.expanduser("~/Projects/datasets/miniCXR8/images/")
+        dataset = CXR8Dataset(datapath, scale_factor=4, add_noise=True)
         print(len(dataset))
         print(dataset.at(0))
 
@@ -89,8 +83,17 @@ if __name__=="__main__":
         imageLR = np.asarray(imageLR[0]) #.transpose(1, 2, 0)
         imageHR = np.asarray(imageHR[0]) #.transpose(1, 2, 0)
         print(imageHR.shape)
+        print(imageLR.max(), imageLR.min())
         import matplotlib.pyplot as plt
-        plt.imshow(imageHR, cmap="gray")
+        plt.style.use('dark_background')
+        fig, axes = plt.subplots(1, 2, figsize=(5, 3), gridspec_kw={'width_ratios': [1, 2]})
+        axes[0].imshow(imageLR, cmap='gray')
+        axes[0].axis('off')
+        axes[0].set_title('Noise')
+        axes[1].imshow(imageHR, cmap='gray')
+        axes[1].axis('off')
+        axes[1].set_title('Xray')
+        fig.tight_layout()
         plt.show()
     elif which == 'Noisy':
         datapath = os.path.expanduser("~/Projects/datasets/xray_images/")
